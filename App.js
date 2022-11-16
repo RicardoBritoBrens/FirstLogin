86edc392f7a79e8508abddc497671d31f1c600a0
Ricardo Brito Brens <britobrensricardo@gmail.com>
2022-11-16 00:21:02 -0400

    Initial Commit

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, ImageBackground, StyleSheet, Text, View, Divider } from 'react-native';
+import colors from './app/configurations/colors';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    featherRegular: require('./app/assets/fonts/Feather/featherRegular.otf'),
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require('./app/assets/women-background.jpeg')} style={styles.container} >
+      <View style={styles.backgroundOverlay} />
+
+      <Text style={styles.title}>DUOLINGUO</Text>
+
+      <Text style={styles.subTitle}>Learn languages from Content You Love</Text>
+
+      {/* devider */}
+      <View style={styles.line} />
+
+      <Text style={styles.slogan}>The Fast, Fun and effective way to Learn</Text>
+
+      <Button style={styles.getStartedButton} title='Get started for free!'></Button>
+      <Button style={styles.alreadyAccountButton} title='I already have an account'></Button>
+    </ImageBackground>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +32,54 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  background: {
+    width: '100%',
+    height: '50%',
+  },
+  backgroundOverlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: colors.primary_background,
+    opacity: 0.85
+  },
+  fontColor: {
+    color: colors.fontColor,
+    fontFamily: 'featherRegular'
+  },
+  title: {
+    color: colors.fontColor,
+    fontFamily: 'featherRegular',
+    fontSize: 32
+  },
+  subTitle: {
+    color: colors.fontColor,
+    fontSize: 26
+  },
+  line: {
+    height: 1, backgroundColor: 'white', width: '70%'
+  },
+  slogan: {
+    color: colors.fontColor,
+  },
+  getStartedButton: {
+    width: '80%',
+    height: 50,
+    borderRadius: 20
+  },
+  alreadyAccountButton: {
+    width: '80%',
+    height: 50,
+    borderRadius: 20
+  }
+
+
 });
+
+
+// https://search.muz.li/NmU2ZDAxYmMy?utm_source=muz.li-insp&utm_medium=article&utm_campaign=%2Finspiration%2Flogin-screen%2F
+// https://docs.expo.dev/guides/using-custom-fonts/
+// https://promo.com/tools/image-resizer/
+// https://html-color-codes.info/colors-from-image/#
---
+++ app/configurations/colors.js
@@ -0,0 +1,8 @@
+export default
+    {
+        primary: '#FBB718',
+        secondary: '#3A4859',
+        primary_background: '#072A59',
+        secondary_background: '#091A2F',
+        fontColor: 'white'
+    }
